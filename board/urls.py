b45f8cd4af976466d72133475af1bc05b7b4d48a
경주원 <oppsjw@naver.com>
2020-08-27 17:03:38 +0900

    first commit

---
+++ board/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    path('index/', views.index),
+    path('', views.index),
+    
+    path('signup/', views.signup),
+    path('signin/', views.signin),
+    path('signout/', views.signout),
+    path('article/write/', views.write),
+    path('article/list/', views.list),
+    path('article/detail/<int:id>/', views.detail),
+    path('article/update/<int:id>/', views.update),
+    path('article/delete/<int:id>/', views.delete),
+    
+    path('test/', views.test),
+]
